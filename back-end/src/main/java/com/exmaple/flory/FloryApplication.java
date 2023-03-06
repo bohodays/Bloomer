3703b2976cb1a10730ab244bb9359b0ca91f2863
찬휘 <cksgnlcjswoo@naver.com>
2023-03-06 10:22:25 +0900

    Feat: setup

---
+++ back-end/src/main/java/com/exmaple/flory/FloryApplication.java
@@ -0,0 +1,13 @@
+package com.exmaple.flory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloryApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/exmaple/flory/FloryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmaple.flory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
