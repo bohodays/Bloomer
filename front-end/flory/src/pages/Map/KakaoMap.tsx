f7dc6b38276619a162dbd1f0c41b30121809beca
Dakyung Seo <seoda0000@gmail.com>
2023-03-10 15:07:55 +0900

    Feat: 현재 좌표 가져오기

--- front-end/flory/src/pages/Map/KakaoMap.tsx
+++ front-end/flory/src/pages/Map/KakaoMap.tsx
@@ -1,14 +1,33 @@
-const { kakao } = window;
+import React, { useEffect } from "react";
+import useGeolocation from "react-hook-geolocation";
 
-function KakaoMap() {
-  const container = document.getElementById("map"); // 지도를 담을 영역의 DOM 레퍼런스
-  const options = {
-    // 지도 생성 기본 옵션
-    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심 좌표
-    level: 3, // 지도의 레벨(확대, 축소 정도)
-  };
-  var map = new kakao.maps.Map(container!, options);
-
-  return <div id="map" style={{ width: "100%", height: "400px" }}></div>;
+declare global {
+  interface Window {
+    kakao: any;
+  }
 }
+
+const KakaoMap = () => {
+  const geolocation = useGeolocation();
+  console.log(geolocation.latitude);
+  console.log(geolocation.longitude);
+
+  useEffect(() => {
+    // 좌표 가져오기
+    let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    let options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(
+        geolocation.latitude,
+        geolocation.longitude
+      ), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  }, [geolocation]);
+
+  return <div id="map" style={{ width: "100vw", height: "300px" }} />;
+};
+
 export default KakaoMap;
