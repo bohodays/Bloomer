a3b7bb7cb43c0ba40569310e73c877f01748281a
Dakyung Seo <seoda0000@gmail.com>
2023-03-13 11:15:33 +0900

    Refactor: styles 분리

--- front-end/flory/src/components/common/Modal/BasicModal.tsx
+++ front-end/flory/src/components/common/Modal/BasicModal.tsx
@@ -1,40 +1,9 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import Button from "@mui/material/Button";
-import Divider from "@mui/material/Divider";
-import { styled } from "@mui/material/styles";
-import { grey } from "@mui/material/colors";
+import { SDrawer, Puller } from "./styles";
 
-// .app에 맞춰 범위 설정
-const StyledSwipeableDrawer = styled(SwipeableDrawer)`
-  .MuiBackdrop-root {
-    position: relative;
-    margin: auto;
-    width: 100%;
-    height: 100%;
-    max-width: 480px;
-  }
-  .MuiPaper-root {
-    margin: auto;
-    width: 100%;
-    max-width: 480px;
-  }
-`;
-
-// Puller 회색 작대기
-const Puller = styled(Box)(({ theme }) => ({
-  width: 50,
-  height: 6,
-  backgroundColor: theme.palette.mode === "light" ? grey[400] : grey[900],
-  borderRadius: 3,
-  position: "absolute",
-  top: 16,
-  bottom: 16,
-  left: "calc(50% - 15px)",
-}));
-
-function Modal({ children }: any): JSX.Element {
+function BasicModal({ children }: any): JSX.Element {
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer =
@@ -61,17 +30,16 @@ function Modal({ children }: any): JSX.Element {
   return (
     <div>
       <Button onClick={toggleDrawer(true)}>bottom</Button>
-      <StyledSwipeableDrawer
+      <SDrawer
         anchor="bottom"
         open={open}
         onClose={toggleDrawer(false)}
         onOpen={toggleDrawer(true)}
-        sx={{ width: "100%" }}
       >
         {ModalContent}
-      </StyledSwipeableDrawer>
+      </SDrawer>
     </div>
   );
 }
 
-export default Modal;
+export default BasicModal;
