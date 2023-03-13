b64185b5760569fb46e06a3d6864c74e3c096d33
jisoo <dlwltn0350@naver.com>
2023-03-13 17:18:22 +0900

    Feat: Member Controller Test

--- back-end/src/test/java/com/exmaple/flory/controller/MemberControllerTest.java
+++ back-end/src/test/java/com/exmaple/flory/controller/MemberControllerTest.java
@@ -1,8 +1,11 @@
 package com.exmaple.flory.controller;
 
+import com.exmaple.flory.dto.member.MemberRequestDto;
 import com.exmaple.flory.dto.member.MemberResponseDto;
+import com.exmaple.flory.dto.team.TeamDto;
 import com.exmaple.flory.entity.Authority;
 import com.exmaple.flory.entity.Member;
+import com.exmaple.flory.response.SuccessResponse;
 import com.exmaple.flory.service.MemberService;
 import com.exmaple.flory.util.SecurityUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,7 +35,8 @@ import java.util.Collections;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -47,6 +52,7 @@ class MemberControllerTest {
 
     private final MemberResponseDto memberResponseDto = MemberResponseDto.builder()
             .userId(1L).nickname("nickname").img("img").email("email").build();
+
     @DisplayName("회원 조회(토큰)")
     @Test
     void findMemberInfoById() throws Exception{
@@ -57,28 +63,98 @@ class MemberControllerTest {
                 .andExpect(status().isOk())
                 .andReturn();
 
-        MemberResponseDto response = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), MemberResponseDto.class);
+        SuccessResponse response = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), SuccessResponse.class);
+        MemberResponseDto result = new ObjectMapper().convertValue(response.getResponse(), MemberResponseDto.class);
 
-        assertThat(response.getEmail()).isEqualTo(memberResponseDto.getEmail());
+        assertThat(result.getEmail()).isEqualTo(memberResponseDto.getEmail());
+    }
+
+    @DisplayName("회원 조회(토큰) 오류")
+    @Test
+    void findMemberInfoByIdException() throws Exception{
+        //given
+        when(memberService.findMemberInfoByUserId(any())).thenThrow(new RuntimeException());
+
+        mockMvc.perform(get("/api/user/me"))
+                .andExpect(status().isInternalServerError())
+                .andReturn();
     }
 
     @DisplayName("회원 조회(이메일)")
     @Test
-    void findMemberInfoByEmail() {
+    void findMemberInfoByEmail() throws Exception{
+        //given
+        when(memberService.findMemberInfoByEmail(any())).thenReturn(memberResponseDto);
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/user/{email}","email"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        SuccessResponse response = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), SuccessResponse.class);
+        MemberResponseDto result = new ObjectMapper().convertValue(response.getResponse(), MemberResponseDto.class);
+
+        assertThat(result.getEmail()).isEqualTo(memberResponseDto.getEmail());
+    }
+
+    @DisplayName("회원 조회(이메일) 오류")
+    @Test
+    void findMemberInfoByEmailException() throws Exception{
+        //given
+        when(memberService.findMemberInfoByEmail(any())).thenThrow(new RuntimeException());
+
+        mockMvc.perform(get("/api/user/{email}","email"))
+                .andExpect(status().isInternalServerError())
+                .andReturn();
     }
 
     @DisplayName("로그아웃")
     @Test
-    void logout() {
+    void logout() throws Exception{
+         mockMvc.perform(get("/api/user/logout"))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 
     @DisplayName("회원 수정")
     @Test
-    void updateMember() {
+    void updateMember() throws Exception{
+        MemberRequestDto memberRequestDto = MemberRequestDto.builder()
+                .nickname("name").password("password").img("img").email("email").build();
+
+        when(memberService.updateMember(any())).thenReturn(memberResponseDto);
+
+        MvcResult mvcResult = mockMvc.perform(put("/api/user").with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(memberRequestDto)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        SuccessResponse response = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), SuccessResponse.class);
+        MemberResponseDto result = new ObjectMapper().convertValue(response.getResponse(), MemberResponseDto.class);
+
+        assertThat(result.getEmail()).isEqualTo(memberResponseDto.getEmail());
+    }
+
+    @DisplayName("회원 수정 오류")
+    @Test
+    void updateMemberException() throws Exception{
+        MemberRequestDto memberRequestDto = MemberRequestDto.builder()
+                .nickname("name").password("password").img("img").email("email").build();
+
+        when(memberService.updateMember(any())).thenThrow(new RuntimeException());
+
+        mockMvc.perform(put("/api/user").with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(memberRequestDto)))
+                .andExpect(status().isInternalServerError())
+                .andReturn();
     }
 
     @DisplayName("회원 탈퇴")
     @Test
-    void deleteMember() {
+    void deleteMember() throws Exception{
+        mockMvc.perform(delete("/api/user/{email}","email").with(csrf()))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 }
