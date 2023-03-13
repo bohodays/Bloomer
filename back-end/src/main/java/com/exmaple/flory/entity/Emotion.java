dbc1d9aa6baf2bf1cc502b8247e99aee8b4c95c5
윤동균 <ydg8732@naver.com>
2023-03-13 15:34:21 +0900

    FEAT: Flower, Emotion 엔티티 생성 및 구조 설정

---
+++ back-end/src/main/java/com/exmaple/flory/entity/Emotion.java
@@ -0,0 +1,28 @@
+package com.exmaple.flory.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name="emotion")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Slf4j
+public class Emotion {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "type")
+    private String type;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "emotion", cascade = CascadeType.ALL)
+    private List<Flower> flowerList;
+}
---
+++ back-end/src/main/java/com/exmaple/flory/entity/Flower.java
@@ -0,0 +1,35 @@
+package com.exmaple.flory.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name="flower")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Slf4j
+public class Flower {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "language")
+    private String language;
+
+    @JoinColumn(name = "eid")
+    @ManyToOne
+    private Emotion emotion;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "flower")
+    private List<Diary> diaryList;
+}
