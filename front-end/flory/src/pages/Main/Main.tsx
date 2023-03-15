ff92d589ceb87cdc1d161b41d1e97b2f516bf923
unknown <pjw2369@naver.com>
2023-03-15 16:36:03 +0900

    Feat: axios 연결

--- front-end/flory/src/pages/Main/Main.tsx
+++ front-end/flory/src/pages/Main/Main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Navbar from "../../components/common/Navbar/Navbar";
+import { logoutAction } from "../../redux/modules/user";
+import { localData } from "../../redux/modules/user/token";
+import { useAppDispatch } from "../../redux/store.hooks";
 import { SMain } from "./styles";
 
 const Main = () => {
+  const dispatch = useAppDispatch();
+
+  // 로그아웃 연습
+  const handleLogout = () => {
+    const accessToken = localData.get("accessToken");
+    dispatch(logoutAction(accessToken)).then(() => {
+      localData.clear();
+    });
+  };
+
   return (
     <SMain>
+      {/* 로그아웃 연습 */}
+      <button onClick={handleLogout}>로그아웃</button>
       {/* 네브바 테스트를 위해 임시로 메인에 넣었습니다. 메인 페이지 작업할 때 빼주세요 */}
       <Navbar />
     </SMain>
