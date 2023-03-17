a7fd4db91d5299e51a70eeba56eace4640ad116f
찬휘 <cksgnlcjswoo@naver.com>
2023-03-17 16:25:51 +0900

    Update: garden test 수정

--- back-end/src/test/java/com/exmaple/flory/repository/GardenRepositoryTest.java
+++ back-end/src/test/java/com/exmaple/flory/repository/GardenRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,6 +23,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @DataJpaTest
 @Import(TestConfig.class)
 @TestPropertySource("classpath:application-test.properties")
+@EnableJpaAuditing
 public class GardenRepositoryTest {
 
     @Autowired
