92a93201839df4246be88cfcdbab028cd51e0831
unknown <pjw2369@naver.com>
2023-03-20 17:45:12 +0900

    Update: 꽃 움직이기 작업 중

--- front-end/flory/src/components/Garden/Base_map_new.tsx
+++ front-end/flory/src/components/Garden/Base_map_new.tsx
@@ -88,7 +88,7 @@ type GLTFResult1 = GLTF & {
 
 export function Model_new(props: JSX.IntrinsicElements["group"]) {
   const { nodes, materials } = useGLTF(
-    "/models/base_map_new.glb"
+    `${process.env.PUBLIC_URL}/models/base_map_new.glb`
   ) as GLTFResult;
   return (
     <group {...props} dispose={null}>
