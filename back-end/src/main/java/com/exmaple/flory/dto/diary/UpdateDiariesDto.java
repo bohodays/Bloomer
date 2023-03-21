863920ff051f21f319ef6ddf22ab6867555e9579
윤동균 <ydg8732@naver.com>
2023-03-21 16:58:41 +0900

    FEAT: Update list dto 추가

---
+++ back-end/src/main/java/com/exmaple/flory/dto/diary/UpdateDiariesDto.java
@@ -0,0 +1,18 @@
+package com.exmaple.flory.dto.diary;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Slf4j
+public class UpdateDiariesDto {
+
+    List<DiaryDto> updateDiaries;
+}
