55c0560ef144db4913ad0a3c6809d50df2c9750e
김정원 <=>
2023-03-22 16:24:47 +0900

    Update: 그룹리스트 화살표 추가

--- front-end/flory/src/pages/GroupList/styles.tsx
+++ front-end/flory/src/pages/GroupList/styles.tsx
@@ -3,36 +3,21 @@ import styled from "styled-components";
 export const SMain = styled.main`
   width: 100%;
   height: 100%;
-  /* margin: auto; */
 
   .search__wrapper {
     width: 90%;
     margin: auto;
-    height: 10%;
-    padding-top: 20px;
+    height: 15%;
+    padding-top: 3.3rem;
   }
 
   .grouplist__wrapper {
     width: 90%;
-    height: 90%;
+    height: 85%;
     margin: auto;
     overflow-y: auto;
-    /* Chrome, Edge, and Safari */
     ::-webkit-scrollbar {
-      width: 0px;
-    }
-    ::-webkit-scrollbar-track {
-      background: #ffffff;
-    }
-    ::-webkit-scrollbar-thumb {
-      background-color: #f2f2fc;
-      border-radius: 10px;
-      border-left: 3px solid #ffffff;
+      display: none;
     }
   }
-
-  /*
-  .grouplist__wrapper:hover {
-    transform: scale(1.03);
-  } */
 `;
