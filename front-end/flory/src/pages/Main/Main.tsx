3147cafb340b6433f907679178f856ddaaedebef
김정원 <=>
2023-03-23 11:19:14 +0900

    Update: 메인 화면 레이아웃 완료

--- front-end/flory/src/pages/Main/Main.tsx
+++ front-end/flory/src/pages/Main/Main.tsx
@@ -7,10 +7,11 @@ import { SMain } from "./styles";
 
 import { ReactComponent as LoadingImg } from "../../assets/imgs/loading.svg";
 import Loading from "../Loading/Loading";
+import { useNavigate } from "react-router";
 
 const Main = () => {
   const dispatch = useAppDispatch();
-
+  const navigate = useNavigate();
   // 로그아웃 연습
   // const handleLogout = () => {
   //   const accessToken = localData.getAccessToken();
@@ -19,12 +20,29 @@ const Main = () => {
   //   });
   // };
 
+  const handleMoveLogin = () => {
+    navigate("/login");
+  };
+  const handleMoveSignUp = () => {
+    navigate("signup");
+  };
+
   return (
     <SMain>
       {/* 로그아웃 연습 */}
-      <Loading />
+      {/* <Loading /> */}
       {/* <button onClick={handleLogout}>로그아웃</button> */}
       {/* 네브바 테스트를 위해 임시로 메인에 넣었습니다. 메인 페이지 작업할 때 빼주세요 */}
+      <h1 className="title">Bloomer</h1>
+      <h3>감정을 꽃 피우다</h3>
+      <div className="btn__wrapper">
+        <button className="btn" onClick={handleMoveLogin}>
+          시작하기
+        </button>
+        {/* <button className="btn" onClick={handleMoveSignUp}>
+          회원가입
+        </button> */}
+      </div>
       <Navbar />
     </SMain>
   );
--- front-end/flory/src/pages/Main/styles.tsx
+++ front-end/flory/src/pages/Main/styles.tsx
@@ -4,4 +4,38 @@ export const SMain = styled.main<any>`
   width: 100%;
   height: 100%;
   background-color: #d7bede;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .title {
+    font-size: 80px;
+    font-family: "Niconne";
+    color: #ffffff;
+    text-align: center;
+    margin-top: 0;
+    text-shadow: #2e2162 5px 4px 5px;
+  }
+
+  .btn__wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+  }
+  .btn {
+    width: 291.92px;
+    height: 57px;
+    background: rgba(255, 255, 255, 0.7);
+    border: 3px solid #ffffff;
+    /* filter: blur(4px); */
+    border-radius: 30px;
+
+    font-size: 20px;
+    font-weight: bolder;
+    color: #612fab;
+    box-shadow: #ffffff 0px 0px 7px 2px;
+  }
 `;
