8a02cff6e69d775f9e4aa015e02bc7f845918cef
jisoo <dlwltn0350@naver.com>
2023-03-24 13:48:48 +0900

    Fix : accessToken 만료 시 인증 방법 수정

--- back-end/src/test/java/com/exmaple/flory/repository/MemberRepositoryTest.java
+++ back-end/src/test/java/com/exmaple/flory/repository/MemberRepositoryTest.java
@@ -122,4 +122,21 @@ class MemberRepositoryTest {
         //then
         assertThat(result).isTrue();
     }
+
+    @DisplayName("refreshToken으로 유저 정보 확인")
+    @Test
+    void findByRefreshToken(){
+        //given
+        Member member = Member.builder()
+                .nickname("nickname").password("password").img("img").email("email") .refreshToken("token").build();
+
+        Member entity = memberRepository.save(member);
+
+        //when
+        Member result = memberRepository.findByRefreshToken(entity.getRefreshToken())
+                .orElseThrow(() -> new RuntimeException("유저 정보가 없습니다."));
+
+        //then
+        assertThat(result.getEmail()).isEqualTo(member.getEmail());
+    }
 }
