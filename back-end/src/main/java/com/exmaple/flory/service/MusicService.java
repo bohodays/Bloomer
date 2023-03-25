3ba68681b0007053f22d5ecc4fb84c125425dcc3
찬휘 <cksgnlcjswoo@naver.com>
2023-03-25 17:20:57 +0900

    Feat: 음악 추천 spring완료

--- back-end/src/main/java/com/exmaple/flory/service/MusicService.java
+++ back-end/src/main/java/com/exmaple/flory/service/MusicService.java
@@ -2,7 +2,12 @@ package com.exmaple.flory.service;
 
 import com.exmaple.flory.dto.music.MusicRecommendResponseDto;
 import com.exmaple.flory.entity.Music;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+
+import net.minidev.json.JSONObject;
+import org.json.JSONArray;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -16,27 +21,36 @@ import java.util.Map;
 @Slf4j
 public class MusicService {
 
-    public List<MusicRecommendResponseDto> recommendMusic(Integer emotion,Long user_id) {
+    public List<MusicRecommendResponseDto> recommendMusic(Integer emotion,Long user_id) throws Exception {
 
         List<MusicRecommendResponseDto> ret = new ArrayList<>();
-
-        String[] emotions = {"joy","relax","embarrassment","rage","unrest","hurt","sad"};
 
         RestTemplate restTemplate = new RestTemplate();
         StringBuilder url = new StringBuilder();
         url.append("http://localhost:8000")
                 .append("/domain/emotions/")
-                .append(emotions[emotion]+"/")
+                .append(emotion+"/")
                 .append("near-user/")
                 .append(user_id);
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>("",headers);
+
         log.info("request domain");
-        String[] response = restTemplate.getForObject(url.toString(), String[].class);
 
-        log.info("1번 : {}\n 2번: {} \n 3번:{}",response[0],response[1],response[2]);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                url.toString(), HttpMethod.GET,entity, String.class);
 
-        for(String str: response) {
-            ret.add(MusicRecommendResponseDto.builder().title(str).build());
+        String responseBody = responseEntity.getBody();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> responseMap = objectMapper.readValue(responseBody, new TypeReference<Map<String,Object>>() {});
+        JSONArray resultArray = new JSONArray((String) responseMap.get("result"));
+
+        for(int i=0;i < resultArray.length();++i) {
+            ret.add(MusicRecommendResponseDto.builder().title(resultArray.getString(i)).build());
         }
 
         return ret;
