41308307998d049a3464a29b0ba8c21d4fab4ddc
JW <wjddnjs0528@gmail.com>
2023-03-29 21:51:16 +0900

    Update: url로 다른 사람 정원 접근 가능

--- front-end/flory/src/pages/GardenOther/GardenOther.tsx
+++ front-end/flory/src/pages/GardenOther/GardenOther.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense, useRef, useEffect, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useRef, useEffect, useState } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
 import {
   Sky,
   Cloud,
@@ -10,22 +10,23 @@ import {
   Float,
   Sparkles,
   Stars,
-} from "@react-three/drei";
-import ToggleButton from "../../components/common/ToggleButton/ToggleButton";
-import Navbar from "../../components/common/Navbar/Navbar";
+} from "@react-three/drei"
+import ToggleButton from "../../components/common/ToggleButton/ToggleButton"
+import Navbar from "../../components/common/Navbar/Navbar"
 
 // import Base_map_new from "../../components/Garden/Park/Park_map";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaintRoller } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
-import Loading from "../Loading/Loading";
-import { useAppDispatch, useAppSelector } from "../../redux/store.hooks";
-import { getDiaryListAction } from "../../redux/modules/diary";
-import Beach_map from "../../components/Garden/Beach/Beach_map";
-import Camp_map from "../../components/Garden/Camp/Camp_map";
-import Park_map from "../../components/Garden/Park/Park_map";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPaintRoller } from "@fortawesome/free-solid-svg-icons"
+import { useLocation, useNavigate } from "react-router-dom"
+import Loading from "../Loading/Loading"
+import { useAppDispatch, useAppSelector } from "../../redux/store.hooks"
+import { getDiaryListAction } from "../../redux/modules/diary"
+import Beach_map from "../../components/Garden/Beach/Beach_map"
+import Camp_map from "../../components/Garden/Camp/Camp_map"
+import Park_map from "../../components/Garden/Park/Park_map"
+import { getCurrentGardenAction } from "../../redux/modules/garden"
 
-let isInitial = true;
+let isInitial = true
 
 const Scene = () => {
   return (
@@ -49,59 +50,58 @@ const Scene = () => {
       </Suspense>
       {/* REPLACE THIS LIGHT AS NEEDED IT'S A GOOD START */}
     </>
-  );
-};
+  )
+}
 
 const GardenOther = () => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+  const location = useLocation()
+  const dispatch = useAppDispatch()
+  const [isExist, setIsExist] = useState(false)
 
-  // 본인 정원 ID
-  const gardenId = useAppSelector((state) => state.garden.gardenData.gardenId);
-  // 본인 ID
-  const requestId = useAppSelector((state) => state.user.userData.userId);
+  // 보고싶은 정원 ID
+  const gardenId = useAppSelector(
+    (state) => state.garden.otherGardenData.gardenId
+  )
+  // 요청하는 user ID
+  const requestId = parseInt(location.pathname.slice(8))
+
+  useEffect(() => {
+    dispatch(getCurrentGardenAction(requestId))
+  }, [])
 
   useEffect(() => {
     const inputData = {
       gardenId,
       requestId,
-    };
-    if (gardenId) {
-      dispatch(getDiaryListAction(inputData));
     }
-  }, [gardenId, dispatch]);
+    if (gardenId) {
+      dispatch(getDiaryListAction(inputData))
+      setIsExist(true)
+    }
+  }, [gardenId, dispatch])
 
   return (
     <>
-      <ToggleButton />
-      <Canvas shadows={true}>
-        {/* REMOVE ORBIT CONTROLS TO FORCE THE CAMERA VIEW */}
-        <OrbitControls
-          maxPolarAngle={Math.PI / 2.8}
-          minZoom={30}
-          maxZoom={200}
-          // 쉬프트 마우스 왼쪽 이동 막는 기능
-          enablePan={false}
-        />
-        <Scene></Scene>
-      </Canvas>
-      {/* 정원 편집 모드 버튼 */}
-      {/* <button
-        onClick={() =>
-          navigate("/garden/edit", {
-            state: {
-              garden: true,
-            },
-          })
-        }
-        className="moveToEdit"
-      >
-        <FontAwesomeIcon icon={faPaintRoller} />
-      </button> */}
-      {/* 네브바 */}
-      {/* <Navbar /> */}
+      {isExist && (
+        <>
+          <ToggleButton state="other" />
+          <Canvas shadows={true}>
+            {/* REMOVE ORBIT CONTROLS TO FORCE THE CAMERA VIEW */}
+            <OrbitControls
+              maxPolarAngle={Math.PI / 2.8}
+              minZoom={30}
+              maxZoom={200}
+              // 쉬프트 마우스 왼쪽 이동 막는 기능
+              enablePan={false}
+            />
+            <Scene></Scene>
+          </Canvas>
+          {/* 네브바 */}
+          <Navbar />
+        </>
+      )}
     </>
-  );
-};
+  )
+}
 
-export default GardenOther;
+export default GardenOther
--- front-end/flory/src/redux/modules/garden/garden-action.ts
+++ front-end/flory/src/redux/modules/garden/garden-action.ts
@@ -1,94 +1,96 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { stringify } from "querystring";
-import { useAppSelector } from "../../store.hooks";
-import { axiosInitializer } from "../../utils/axiosInitializer";
-import { localData } from "../user/token";
-import { gardenActions } from "./garden-slice";
+import { createAsyncThunk } from "@reduxjs/toolkit"
+import { stringify } from "querystring"
+import { useAppSelector } from "../../store.hooks"
+import { axiosInitializer } from "../../utils/axiosInitializer"
+import { localData } from "../user/token"
+import { gardenActions } from "./garden-slice"
 
-// 현재 정원 확인
+// 유저별 현재 정원 확인
 export const getCurrentGardenAction = createAsyncThunk(
   "GET_CURRENT_GARDEN",
-  async (_, { dispatch, rejectWithValue }) => {
+  async (userId: number, { dispatch, rejectWithValue }) => {
     try {
-      let today = new Date();
-      let year = today.getFullYear(); // 년도
-      let month = today.getMonth() + 1; // 월
-      const accessToken = localData.getAccessToken();
-      const axios = axiosInitializer();
-      const { data } = await axios.get(`/api/garden/date/2023/${month}`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      console.log(data, "현재 정원 확인");
+      let today = new Date()
+      let year = today.getFullYear() // 년도
+      let month = today.getMonth() + 1 // 월
+      const accessToken = localData.getAccessToken()
+      const axios = axiosInitializer()
+      const { data } = await axios.get(
+        `/api/garden/user/${userId}/date/${year}/${month}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
 
-      return data;
+      return data
     } catch (e: any) {
-      console.log("오잉?");
-      const user = useAppSelector((state) => state.user.userData);
-      console.log(user);
-      return rejectWithValue(e);
+      console.log("오잉?")
+      const user = useAppSelector((state) => state.user.userData)
+      console.log(user)
+      return rejectWithValue(e)
     }
   }
-);
+)
 
 // 정원 목록 확인
 export const getGardenListAction = createAsyncThunk(
   "GET_GARDEN_LIST",
   async (userId: number, { dispatch, rejectWithValue }) => {
     try {
-      const accessToken = localData.getAccessToken();
-      const axios = axiosInitializer();
+      const accessToken = localData.getAccessToken()
+      const axios = axiosInitializer()
       const { data } = await axios.get(`/api/garden/list/${userId}`, {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${accessToken}`,
         },
-      });
+      })
       if (data.response.length > 0) {
         const latestDate = data.response[
           data.response.length - 1
-        ].deadline.substr(0, 7);
-        let today = new Date();
-        let year = String(today.getFullYear()); // 년도
-        let month = String(today.getMonth() + 1); // 월
+        ].deadline.substr(0, 7)
+        let today = new Date()
+        let year = String(today.getFullYear()) // 년도
+        let month = String(today.getMonth() + 1) // 월
         if (month.length < 2) {
-          month = "0" + String(month);
+          month = "0" + String(month)
         }
 
         if (latestDate !== String(year) + "-" + String(month)) {
-          console.log("최신 아닙니다");
-          dispatch(createGardenAction(userId));
+          console.log("최신 아닙니다")
+          dispatch(createGardenAction(userId))
         } else {
-          const currentGarden = data.response[data.response.length - 1];
+          const currentGarden = data.response[data.response.length - 1]
           dispatch(
             gardenActions.setGardenData({
               gardenData: currentGarden,
             })
-          );
+          )
         }
       } else {
-        console.log("정원 신규 생성");
-        dispatch(createGardenAction(userId));
+        console.log("정원 신규 생성")
+        dispatch(createGardenAction(userId))
       }
 
-      return data;
+      return data
     } catch (e: any) {
-      return rejectWithValue(e);
+      return rejectWithValue(e)
     }
   }
-);
+)
 
 // 정원 생성
 export const createGardenAction = createAsyncThunk(
   "CREATE",
   async (userId: number, { dispatch, rejectWithValue }) => {
     try {
-      console.log("정원 생성 시도합니다");
-      const accessToken = localData.getAccessToken();
-      const axios = axiosInitializer();
-      console.log(userId);
+      console.log("정원 생성 시도합니다")
+      const accessToken = localData.getAccessToken()
+      const axios = axiosInitializer()
+      console.log(userId)
       const { data } = await axios.post(
         `/api/garden`,
         {
@@ -100,12 +102,12 @@ export const createGardenAction = createAsyncThunk(
             Authorization: `Bearer ${accessToken}`,
           },
         }
-      );
+      )
       // const userId = useAppSelector((state) => state.user.userData.userId);
-      dispatch(getGardenListAction(userId));
-      return data;
+      dispatch(getGardenListAction(userId))
+      return data
     } catch (e: any) {
-      return rejectWithValue(e);
+      return rejectWithValue(e)
     }
   }
-);
+)
