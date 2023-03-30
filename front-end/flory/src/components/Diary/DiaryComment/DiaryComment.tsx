4fd056041cb3a2c5be62daf6353754a88d2876c0
Dakyung Seo <seoda0000@gmail.com>
2023-03-30 10:24:38 +0900

    Feat: 설정 popover

--- front-end/flory/src/components/Diary/DiaryComment/DiaryComment.tsx
+++ front-end/flory/src/components/Diary/DiaryComment/DiaryComment.tsx
@@ -1,13 +1,18 @@
-import { useNavigate } from "react-router-dom"
-import { JsxElement } from "typescript"
-import Avatar from "../../common/Avatar/Avatar"
-import { SMain } from "./styles"
+import { useNavigate } from "react-router-dom";
+import { JsxElement } from "typescript";
+import Avatar from "../../common/Avatar/Avatar";
+import SettingPopover from "../../common/SettingPopover/SettingPopover";
+
+import { SMain } from "./styles";
 function DiaryComment({ comment }: any): JSX.Element {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleMoveToOtherGarden = () => {
-    navigate(`/garden/${comment.member.userId}`)
-  }
+    navigate(`/garden/${comment.member.userId}`);
+  };
+
+  const editAction = () => {};
+  const deleteAction = () => {};
 
   return (
     <SMain style={{ display: "flex", flexDirection: "column" }}>
@@ -19,6 +24,7 @@ function DiaryComment({ comment }: any): JSX.Element {
         >
           {comment.member.nickname}
         </p>
+        <SettingPopover editAction={editAction} deleteAction={deleteAction} />
       </div>
       <p>{comment.content}</p>
       <p
@@ -34,7 +40,7 @@ function DiaryComment({ comment }: any): JSX.Element {
           comment.createdTime.slice(11, 16)}
       </p>
     </SMain>
-  )
+  );
 }
 
-export default DiaryComment
+export default DiaryComment;
