0d2169d11a044b13b1f91f44731572c7bab671f4
unknown <pjw2369@naver.com>
2023-03-31 09:24:11 +0900

    Update: AOS 제거

--- front-end/flory/src/components/GuestBook/GuestBookComment/GuestBookComment.tsx
+++ front-end/flory/src/components/GuestBook/GuestBookComment/GuestBookComment.tsx
@@ -1,76 +1,43 @@
-import React, { useEffect, useRef } from "react"
-import Avatar from "../../common/Avatar/Avatar"
-import { SSection } from "./styles"
-import AOS from "aos"
-import "aos/dist/aos.css"
-import { convertDateFormat } from "../../../utils/utils"
-import { useAppDispatch, useAppSelector } from "../../../redux/store.hooks"
-import SettingPopover from "../../common/SettingPopover/SettingPopover"
+import React, { useEffect, useRef } from "react";
+import Avatar from "../../common/Avatar/Avatar";
+import { SSection } from "./styles";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { convertDateFormat } from "../../../utils/utils";
+import { useAppDispatch, useAppSelector } from "../../../redux/store.hooks";
+import SettingPopover from "../../common/SettingPopover/SettingPopover";
 import {
   deleteGuestBook,
   getAllGuestBookList,
-} from "../../../redux/modules/guestBook"
+} from "../../../redux/modules/guestBook";
+import "animate.css";
 
 const GuestBookComment = (props: any) => {
-  const commentRef = useRef<HTMLElement>(null)
-  const dispatch = useAppDispatch()
-  const commentData = props.info
+  const commentRef = useRef<HTMLElement>(null);
+  const dispatch = useAppDispatch();
+  const commentData = props.info;
 
-  const userId: number = useAppSelector((state) => state.user.userData.userId)
-  const writerId: number = commentData.userId
-  const isSelf: boolean = userId === writerId ? true : false
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          AOS.init()
-          observer.unobserve(entry.target)
-        }
-      })
-    })
-    if (commentRef.current) {
-      observer.observe(commentRef.current)
-    }
-
-    const observer2 = new MutationObserver((mutationsList) => {
-      for (const mutation of mutationsList) {
-        if (mutation.attributeName === "data-aos") {
-          if (
-            (mutation.target as HTMLElement).getAttribute("data-aos") === null
-          ) {
-            AOS.refresh()
-          }
-        }
-      }
-    })
-    if (commentRef.current) {
-      observer2.observe(commentRef.current, { attributes: true })
-    }
-
-    return () => {
-      observer.disconnect()
-      observer2.disconnect()
-    }
-  }, [])
+  const userId: number = useAppSelector((state) => state.user.userData.userId);
+  const writerId: number = commentData.userId;
+  const isSelf: boolean = userId === writerId ? true : false;
 
   const checkDeg = (num: number) => {
-    if (num > 0) return "fade-left"
-    else if (num === 0) return "fade-down"
-    else if (num < 0) return "fade-right"
-  }
+    if (num > 0) return "animate__animated animate__fadeInLeft";
+    else if (num === 0) return "animate__animated animate__fadeInDown";
+    else if (num < 0) return "animate__animated animate__fadeInRight";
+  };
 
   const handleDelete = () => {
     const requestData = {
       bookId: commentData.book_id,
       gardenId: commentData.gardenId,
-    }
-    dispatch(deleteGuestBook(requestData))
-  }
+    };
+    dispatch(deleteGuestBook(requestData));
+  };
 
   return (
     <SSection deg={props.deg} ref={commentRef} color={commentData.color}>
-      <div className="post-it" data-aos={checkDeg(props.deg)}>
+      <div className={`post-it ${checkDeg(props.deg)}`}>
         <p className="note">
           <div className="header">
             {/* 작성자 프로필 이미지 */}
@@ -89,7 +56,7 @@ const GuestBookComment = (props: any) => {
         </p>
       </div>
     </SSection>
-  )
-}
+  );
+};
 
-export default GuestBookComment
+export default GuestBookComment;
