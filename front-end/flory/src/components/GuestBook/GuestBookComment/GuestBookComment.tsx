4007bd65309b41b04f32f493542a7df692561c12
김정원 <=>
2023-03-31 11:08:37 +0900

    Update: 수정 로직 삭제

--- front-end/flory/src/components/GuestBook/GuestBookComment/GuestBookComment.tsx
+++ front-end/flory/src/components/GuestBook/GuestBookComment/GuestBookComment.tsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useRef } from "react"
-import Avatar from "../../common/Avatar/Avatar"
-import { SSection } from "./styles"
-import AOS from "aos"
-import "aos/dist/aos.css"
-import { convertDateFormat } from "../../../utils/utils"
-import { useAppDispatch, useAppSelector } from "../../../redux/store.hooks"
-import SettingPopover from "../../common/SettingPopover/SettingPopover"
+import React, { useEffect, useRef } from "react";
+import Avatar from "../../common/Avatar/Avatar";
+import { SSection } from "./styles";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { convertDateFormat } from "../../../utils/utils";
+import { useAppDispatch, useAppSelector } from "../../../redux/store.hooks";
+import SettingPopover from "../../common/SettingPopover/SettingPopover";
 import {
   deleteGuestBook,
   getAllGuestBookList,
-} from "../../../redux/modules/guestBook"
+} from "../../../redux/modules/guestBook";
 
 const GuestBookComment = (props: any) => {
-  const commentRef = useRef<HTMLElement>(null)
-  const dispatch = useAppDispatch()
-  const commentData = props.info
+  const commentRef = useRef<HTMLElement>(null);
+  const dispatch = useAppDispatch();
+  const commentData = props.info;
 
-  const userId: number = useAppSelector((state) => state.user.userData.userId)
-  const writerId: number = commentData.userId
-  const isSelf: boolean = userId === writerId ? true : false
+  const userId: number = useAppSelector((state) => state.user.userData.userId);
+  const writerId: number = commentData.userId;
+  const isSelf: boolean = userId === writerId ? true : false;
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          AOS.init()
-          observer.unobserve(entry.target)
+          AOS.init();
+          observer.unobserve(entry.target);
         }
-      })
-    })
+      });
+    });
     if (commentRef.current) {
-      observer.observe(commentRef.current)
+      observer.observe(commentRef.current);
     }
 
     const observer2 = new MutationObserver((mutationsList) => {
@@ -39,34 +39,34 @@ const GuestBookComment = (props: any) => {
           if (
             (mutation.target as HTMLElement).getAttribute("data-aos") === null
           ) {
-            AOS.refresh()
+            AOS.refresh();
           }
         }
       }
-    })
+    });
     if (commentRef.current) {
-      observer2.observe(commentRef.current, { attributes: true })
+      observer2.observe(commentRef.current, { attributes: true });
     }
 
     return () => {
-      observer.disconnect()
-      observer2.disconnect()
-    }
-  }, [])
+      observer.disconnect();
+      observer2.disconnect();
+    };
+  }, []);
 
   const checkDeg = (num: number) => {
-    if (num > 0) return "fade-left"
-    else if (num === 0) return "fade-down"
-    else if (num < 0) return "fade-right"
-  }
+    if (num > 0) return "fade-left";
+    else if (num === 0) return "fade-down";
+    else if (num < 0) return "fade-right";
+  };
 
   const handleDelete = () => {
     const requestData = {
       bookId: commentData.book_id,
       gardenId: commentData.gardenId,
-    }
-    dispatch(deleteGuestBook(requestData))
-  }
+    };
+    dispatch(deleteGuestBook(requestData));
+  };
 
   return (
     <SSection deg={props.deg} ref={commentRef} color={commentData.color}>
@@ -89,7 +89,7 @@ const GuestBookComment = (props: any) => {
         </p>
       </div>
     </SSection>
-  )
-}
+  );
+};
 
-export default GuestBookComment
+export default GuestBookComment;
