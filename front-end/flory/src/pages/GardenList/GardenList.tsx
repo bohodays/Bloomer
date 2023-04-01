4fc0a9478fc4da3d43403d0e86e25053d11b99f6
김정원 <wjddnjs0528@gmail.com>
2023-04-02 00:36:13 +0900

    Fix: 일기리스트 api 요청 더미데이터 수정

--- front-end/flory/src/pages/GardenList/GardenList.tsx
+++ front-end/flory/src/pages/GardenList/GardenList.tsx
@@ -1,34 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { SMain } from "./styles";
-import { TiChevronRightOutline, TiChevronLeftOutline } from "react-icons/ti";
-import testGarden from "../../assets/imgs/test_garden.png";
-import Button from "../../components/common/Button/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { useLocation, useNavigate } from "react-router-dom";
-import GardenCalendar from "../../components/Garden/GardenCalendar/GardenCalendar";
-import BackButton from "../../components/common/BackButton/BackButton";
-import { useSelect } from "@react-three/drei";
-import { useAppDispatch, useAppSelector } from "../../redux/store.hooks";
-import { getDiaryWithDate } from "../../redux/modules/diary";
-import Tooltip from "@mui/material/Tooltip";
+import React, { useState, useEffect } from "react"
+import { SMain } from "./styles"
+import { TiChevronRightOutline, TiChevronLeftOutline } from "react-icons/ti"
+import testGarden from "../../assets/imgs/test_garden.png"
+import Button from "../../components/common/Button/Button"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
+import { useLocation, useNavigate } from "react-router-dom"
+import GardenCalendar from "../../components/Garden/GardenCalendar/GardenCalendar"
+import BackButton from "../../components/common/BackButton/BackButton"
+import { useSelect } from "@react-three/drei"
+import { useAppDispatch, useAppSelector } from "../../redux/store.hooks"
+import { getDiaryWithDate } from "../../redux/modules/diary"
+import Tooltip from "@mui/material/Tooltip"
+import { convertGardenTheme } from "../../utils/utils"
 
 // const CARDS = 1;
-const MAX_VISIBILITY = 3;
+const MAX_VISIBILITY = 3
 
 const Carousel = ({ children, setActiveIdx, activeIdx }: any) => {
-  const count = React.Children.count(children);
-  const [active, setActive] = useState<number>(count - 1);
+  const count = React.Children.count(children)
+  const [active, setActive] = useState<number>(count - 1)
 
   const handleClickRight = () => {
-    setActive((i) => i + 1);
-    setActiveIdx(activeIdx + 1);
-  };
+    setActive((i) => i + 1)
+    setActiveIdx(activeIdx + 1)
+  }
 
   const handleClickLeft = () => {
-    setActive((i) => i - 1);
-    setActiveIdx(activeIdx - 1);
-  };
+    setActive((i) => i - 1)
+    setActiveIdx(activeIdx - 1)
+  }
 
   return (
     <>
@@ -65,40 +66,42 @@ const Carousel = ({ children, setActiveIdx, activeIdx }: any) => {
         )}
       </div>
     </>
-  );
-};
+  )
+}
 
 const GardenList = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const backpage = location.state ? location.state.gid : null;
-  const dispatch = useAppDispatch();
-  const gardenList = useAppSelector((state) => state.garden.gardenList);
-  const userId = useAppSelector((state) => state.user.userData.userId);
-  const CARDS_LENGTH = gardenList.length;
-  const [activeIdx, setActiveIdx] = useState(CARDS_LENGTH - 1);
-  const target = new Date(gardenList[activeIdx].deadline);
-  const year = target.getFullYear();
-  const month = target.getMonth() + 1;
-  const monthDiaryList = useAppSelector((state) => state.diary.monthDiaryList);
+  const navigate = useNavigate()
+  const location = useLocation()
+  const dispatch = useAppDispatch()
+  const backpage = location.state ? location.state.gid : null
+  const gardenList = useAppSelector((state) => state.garden.gardenList)
+  const userId = useAppSelector((state) => state.user.userData.userId)
+  const CARDS_LENGTH = gardenList.length
+  const [activeIdx, setActiveIdx] = useState(CARDS_LENGTH - 1)
+  const target = gardenList.length
+    ? new Date(gardenList[activeIdx].deadline)
+    : null
+  const year = target ? target.getFullYear() : null
+  const month = target ? target.getMonth() + 1 : null
+  const monthDiaryList = useAppSelector((state) => state.diary.monthDiaryList)
   const dateData = {
     // 조회할 사용자 유저 아이디
     id: userId,
     year,
     month,
-  };
+  }
 
   const handleMoveToGarden = (
     year: number,
     month: number,
     gid: number | null
   ) => {
-    navigate("/garden", { state: { path: "garden/list", year, month, gid } });
-  };
+    navigate("/garden", { state: { path: "garden/list", year, month, gid } })
+  }
 
   useEffect(() => {
-    dispatch(getDiaryWithDate(dateData));
-  }, [activeIdx]);
+    dispatch(getDiaryWithDate(dateData))
+  }, [activeIdx])
 
   return (
     <SMain>
@@ -106,50 +109,59 @@ const GardenList = () => {
         color="purple"
         onClickAction={() => {
           if (backpage) {
-            navigate(-1);
+            navigate(-1)
           } else {
-            navigate("/garden");
+            navigate("/garden")
           }
         }}
       />
-
-      <div className="info__wrapper">
-        <div className="background">
-          {year}년 {month}월
-        </div>
-      </div>
-      {/* 기간별 정원 */}
-      <div className="wrapper">
-        <Carousel setActiveIdx={setActiveIdx} activeIdx={activeIdx}>
-          {gardenList.map((_, i) => (
-            <Tooltip
-              title={`${year.toString().slice(2)}년 ${month}월 공간 보러가기`}
-              placement="bottom"
-            >
-              {/* 이미지 바껴야 함 */}
-              <img
-                style={{ cursor: "pointer" }}
-                src={testGarden}
-                alt=""
-                key={i}
-                onClick={() =>
-                  handleMoveToGarden(year, month, gardenList[i].gardenId)
-                }
-              />
-            </Tooltip>
-          ))}
-        </Carousel>
-      </div>
-      <Tooltip
-        title={`${year.toString().slice(2)}년 ${month}월 일기 보러가기`}
-        placement="bottom"
-      >
-        <div className="calendar__wrapper">
-          <GardenCalendar dateData={dateData} diaryList={monthDiaryList} />
-        </div>
-      </Tooltip>
+      {target && year && month ? (
+        <>
+          <div className="info__wrapper">
+            <div className="background">
+              {year}년 {month}월
+            </div>
+          </div>
+          {/* 기간별 정원 */}
+          <div className="wrapper">
+            <Carousel setActiveIdx={setActiveIdx} activeIdx={activeIdx}>
+              {gardenList.map((item, i) => (
+                <Tooltip
+                  title={`${year
+                    .toString()
+                    .slice(2)}년 ${month}월 감정 공간 보러가기`}
+                  placement="bottom"
+                  arrow
+                  style={{ backgroundColor: "red" }}
+                >
+                  {/* 이미지 바껴야 함 */}
+                  <img
+                    src={convertGardenTheme(item.type)}
+                    alt=""
+                    key={i}
+                    onClick={() =>
+                      handleMoveToGarden(year, month, gardenList[i].gardenId)
+                    }
+                  />
+                </Tooltip>
+              ))}
+            </Carousel>
+          </div>
+          <Tooltip
+            title={`${year.toString().slice(2)}년 ${month}월 일기 보러가기`}
+            placement="bottom"
+            arrow
+          >
+            <div className="calendar__wrapper">
+              <GardenCalendar dateData={dateData} diaryList={monthDiaryList} />
+            </div>
+          </Tooltip>
+        </>
+      ) : (
+        <div>잘못된 접근입니다</div>
+      )}
     </SMain>
-  );
-};
+  )
+}
 
-export default GardenList;
+export default GardenList
