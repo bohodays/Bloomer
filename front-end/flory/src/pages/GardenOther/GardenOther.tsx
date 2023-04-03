d37181ff75b0b077403e2dd9bbced13dec6fa506
김정원 <wjddnjs0528@gmail.com>
2023-04-03 22:20:55 +0900

    Update: 공개 범위별로 일기 열람 가능 로직 추가

--- front-end/flory/src/pages/GardenOther/GardenOther.tsx
+++ front-end/flory/src/pages/GardenOther/GardenOther.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense, useRef, useEffect, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useRef, useEffect, useState } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
 import {
   Sky,
   Cloud,
@@ -10,34 +10,34 @@ import {
   Float,
   Sparkles,
   Stars,
-} from "@react-three/drei";
-import ToggleButton from "../../components/common/ToggleButton/ToggleButton";
-import Navbar from "../../components/common/Navbar/Navbar";
+} from "@react-three/drei"
+import ToggleButton from "../../components/common/ToggleButton/ToggleButton"
+import Navbar from "../../components/common/Navbar/Navbar"
 
 // import Base_map_new from "../../components/Garden/Park/Park_map";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaintRoller } from "@fortawesome/free-solid-svg-icons";
-import { useLocation, useNavigate } from "react-router-dom";
-import Loading from "../Loading/Loading";
-import { useAppDispatch, useAppSelector } from "../../redux/store.hooks";
-import { getDiaryListAction } from "../../redux/modules/diary";
-import Beach_map from "../../components/Garden/Beach/Beach_map";
-import Camp_map from "../../components/Garden/Camp/Camp_map";
-import Park_map from "../../components/Garden/Park/Park_map";
-import { getCurrentGardenAction } from "../../redux/modules/garden";
-import BackButton from "../../components/common/BackButton/BackButton";
-import { Translate } from "aws-sdk";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPaintRoller } from "@fortawesome/free-solid-svg-icons"
+import { useLocation, useNavigate } from "react-router-dom"
+import Loading from "../Loading/Loading"
+import { useAppDispatch, useAppSelector } from "../../redux/store.hooks"
+import { getDiaryListAction } from "../../redux/modules/diary"
+import Beach_map from "../../components/Garden/Beach/Beach_map"
+import Camp_map from "../../components/Garden/Camp/Camp_map"
+import Park_map from "../../components/Garden/Park/Park_map"
+import { getCurrentGardenAction } from "../../redux/modules/garden"
+import BackButton from "../../components/common/BackButton/BackButton"
+import { Translate } from "aws-sdk"
 
-let isInitial = true;
+let isInitial = true
 
 const gardenTypeMap = (type: number | null) => {
-  if (type === 0) return <Park_map />;
-  else if (type === 1) return <Camp_map />;
-  else if (type === 2) return <Beach_map />;
-};
+  if (type === 0) return <Park_map page="other" />
+  else if (type === 1) return <Camp_map page="other" />
+  else if (type === 2) return <Beach_map page="other" />
+}
 
 const Scene = (props: any) => {
-  const otherGardenType = props.otherGardenType;
+  const otherGardenType = props.otherGardenType
 
   return (
     <>
@@ -61,48 +61,48 @@ const Scene = (props: any) => {
       </Suspense>
       {/* REPLACE THIS LIGHT AS NEEDED IT'S A GOOD START */}
     </>
-  );
-};
+  )
+}
 
 const GardenOther = () => {
-  const location = useLocation();
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const [isExist, setIsExist] = useState(false);
+  const location = useLocation()
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const [isExist, setIsExist] = useState(false)
   const otherGardenData = useAppSelector(
     (state) => state.garden.otherGardenData
-  );
+  )
 
   // 보고싶은 정원의 테마 type
-  const otherGardenType = otherGardenData.type;
+  const otherGardenType = otherGardenData.type
 
   // 보고싶은 정원 ID
-  const gardenId = otherGardenData.gardenId;
+  const gardenId = otherGardenData.gardenId
   // 보고싶은 user ID
-  const requestId = parseInt(location.pathname.slice(8));
+  const requestId = parseInt(location.pathname.slice(8))
 
   const handleMoveToGuestBook = () => {
     navigate("/guestbook", {
       state: {
         gardenData: otherGardenData,
       },
-    });
-  };
+    })
+  }
 
   useEffect(() => {
-    dispatch(getCurrentGardenAction(requestId));
-  }, []);
+    dispatch(getCurrentGardenAction(requestId))
+  }, [])
 
   useEffect(() => {
     const inputData = {
       gardenId,
       requestId,
-    };
-    if (gardenId) {
-      dispatch(getDiaryListAction(inputData));
-      setIsExist(true);
     }
-  }, [gardenId, dispatch]);
+    if (gardenId) {
+      dispatch(getDiaryListAction(inputData))
+      setIsExist(true)
+    }
+  }, [gardenId, dispatch])
 
   return (
     <>
@@ -139,7 +139,7 @@ const GardenOther = () => {
         </>
       )}
     </>
-  );
-};
+  )
+}
 
-export default GardenOther;
+export default GardenOther
