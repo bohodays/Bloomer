ca8db72cfc5b15a22eb0a939202e3da4fe3a9d2f
Dakyung Seo <seoda0000@gmail.com>
2023-04-04 15:45:31 +0900

    Update: 메인페이지 폰트 변경

--- front-end/flory/src/index.tsx
+++ front-end/flory/src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux"
-import store from "./redux/store"
-import App from "./App"
-import "./index.css"
-import googleLogo from "../src/assets/imgs/googlePlayLogo.png"
-import QR from "../src/assets/imgs/QR.png"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./App";
+import "./index.css";
+import googleLogo from "../src/assets/imgs/googlePlayLogo.png";
+import QR from "../src/assets/imgs/QR.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-const container = document.getElementById("root")!
-const root = createRoot(container)
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
@@ -18,7 +18,7 @@ root.render(
       <div className="wrapper">
         <span className="text__wrapper">
           <span id="main-title">Bloomer</span>
-          <span id="sub-title">감정을 꽃 피우다</span>
+          <span id="sub-title">감정을 꽃 피우다 </span>
         </span>
         <div className="google__wrapper">
           <img id="google-logo" src={googleLogo} />
@@ -38,7 +38,7 @@ root.render(
       </div>
     </Provider>
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
