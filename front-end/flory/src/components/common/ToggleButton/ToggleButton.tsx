91a879ef2a094bcaa700c199fec1295e6c475e94
김정원 <wjddnjs0528@gmail.com>
2023-04-04 23:24:19 +0900

    Update: 화면캡처 시도중

--- front-end/flory/src/components/common/ToggleButton/ToggleButton.tsx
+++ front-end/flory/src/components/common/ToggleButton/ToggleButton.tsx
@@ -5,79 +5,116 @@ import {
   faVolumeOff,
   faVolumeXmark,
   faXmark,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-
-
-
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../../redux/store.hooks";
-import { SNav } from "./styles";
-import { updateIsPlaying } from "../../../redux/modules/music/music-slice";
+import React, { useState, useEffect, RefObject } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAppDispatch, useAppSelector } from "../../../redux/store.hooks"
+import { SNav } from "./styles"
+import { updateIsPlaying } from "../../../redux/modules/music/music-slice"
+import html2canvas, { Options } from "html2canvas"
 
 const ToggleButton = ({ state, gardenType, capture }: any) => {
-  const [toggleOnOff, setToggleOnOff] = useState(false);
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const music = useAppSelector((store) => store.music);
-  const [volumeMute, setVolumeMute] = useState(false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const [toggleOnOff, setToggleOnOff] = useState(false)
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+  const music = useAppSelector((store) => store.music)
+  const [volumeMute, setVolumeMute] = useState(false)
 
   // 토글 핸들러 함수
   const handleToggleOnOff = () => {
-    setToggleOnOff(!toggleOnOff);
-  };
+    setToggleOnOff(!toggleOnOff)
+  }
 
   // 소리 음소거 핸들러 함수
   const handleVolumeOnOff = () => {
-
-
-
-
-    console.log("볼륨 조정", volumeMute);
+    console.log("볼륨 조정", volumeMute)
     if (music.isPlaying) {
-      dispatch(updateIsPlaying(false));
-      setVolumeMute(true);
+      dispatch(updateIsPlaying(false))
+      setVolumeMute(true)
     } else {
-      dispatch(updateIsPlaying(true));
-      setVolumeMute(false);
+      dispatch(updateIsPlaying(true))
+      setVolumeMute(false)
     }
-  };
+  }
 
   useEffect(() => {
     if (music.isPlaying) {
-      setVolumeMute(true);
+      setVolumeMute(true)
     } else {
-      setVolumeMute(false);
+      setVolumeMute(false)
     }
-  }, [music]);
-
-
-
-
-
+  }, [music])
 
   const handleMoveToGardenList = () => {
-    navigate("/garden/list");
-  };
+    navigate("/garden/list")
+  }
+
+  type DownloadOptions = {
+    fileName: string
+    imageQuality?: number
+    canvasOptions?: Partial<Options>
+  }
+
+  const defaultOptions: DownloadOptions = {
+    fileName: "default",
+    imageQuality: 1,
+    canvasOptions: {},
+  }
+
+  const IMAGETYPE = "image/png"
+
+  const download = (url: string, filename: string) => {
+    const linkElement = document.createElement("a")
+    linkElement.download = filename
+    linkElement.href = url
+    linkElement.style.display = "none"
+
+    document.body.appendChild(linkElement)
+    linkElement.click()
+    document.body.removeChild(linkElement)
+  }
+
+  const handleScreenShot = async (
+    element: RefObject<HTMLElement> | null,
+    options: DownloadOptions
+  ) => {
+    if (!element || !element.current) console.log("결과가 존재하지 않습니다.")
+
+    const { fileName, imageQuality, canvasOptions } = options
+    try {
+      const appElement = document.getElementsByClassName(
+        "app"
+      )[0]! as HTMLElement
+      const canvas = await html2canvas(appElement, {
+        ...defaultOptions,
+        ...canvasOptions,
+        useCORS: true,
+      })
+
+      // const canvas = await html2canvas(element?.current!, {
+      //   ...defaultOptions,
+      //   ...canvasOptions,
+      //   useCORS: true,
+      // })
+
+      console.log("jjjj", appElement)
+
+      download(canvas.toDataURL(IMAGETYPE, imageQuality), fileName)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // const handleScreenShot = () => {
+  //   console.log("카메라 눌림")
+
+  //   const appElement = document.getElementsByClassName("app")[0]! as HTMLElement
+  //   html2canvas(appElement).then((canvas) => {
+  //     console.log(canvas)
+  //   })
+  // }
 
   return (
     <SNav gardenType={gardenType}>
@@ -114,11 +151,11 @@ const ToggleButton = ({ state, gardenType, capture }: any) => {
           <FontAwesomeIcon className="volumeMute__icon" icon={faVolumeXmark} />
         )}
       </button>
-      <button className="menu-item camera" onClick={capture}>
+      <button className="menu-item camera" onClick={(e) => handleScreenShot}>
         <FontAwesomeIcon className="camera__icon" icon={faCamera} />
       </button>
     </SNav>
-  );
-};
+  )
+}
 
-export default ToggleButton;
+export default ToggleButton
