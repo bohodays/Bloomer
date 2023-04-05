403ef1dde19dec96766013e704a45257f3e73d22
Dakyung Seo <seoda0000@gmail.com>
2023-04-05 14:56:45 +0900

    Feat: 이미지 저장 시도

--- front-end/flory-app/App.js
+++ front-end/flory-app/App.js
@@ -11,6 +11,8 @@ import {
 import { WebView } from "react-native-webview";
 import { BackHandler } from "react-native";
 import * as Location from "expo-location";
+import * as MediaLibrary from "expo-media-library";
+import * as FileSystem from "expo-file-system";
 
 export default function App() {
   // 로딩페이지 관련
@@ -82,6 +84,41 @@ export default function App() {
     };
   }, [AppState]);
 
+  // save image
+  const SaveToPhone = async (url) => {
+    console.log(url);
+    Toast.show("image download", {
+      duration: 1000,
+    });
+    const { uri } = await FileSystem.downloadAsync(
+      url,
+      `${FileSystem.documentDirectory}meme.jpg`
+    ).catch((e) =>
+      console.log("instagram share failed", JSON.stringify(e), url)
+    );
+
+    const permission = await MediaLibrary.requestPermissionsAsync();
+    if (permission.granted) {
+      try {
+        const asset = await MediaLibrary.createAssetAsync(uri);
+        MediaLibrary.createAlbumAsync("Images", asset, false)
+          .then(() => {
+            console.log("File Saved Successfully!");
+            Toast.show("image save success!!", {
+              duration: 1000,
+            });
+          })
+          .catch(() => {
+            console.log("Error In Saving File!");
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Need Storage permission to save file");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={{ width: "100%", height: "100%" }}>
@@ -93,6 +130,12 @@ export default function App() {
           domStorageEnabled={true}
           geolocationEnabled={true}
           style={{ zIndex: 1 }}
+          useWebkit
+          // 캡쳐 저장 관련
+          allowFileAccess={true}
+          allowFileAccessFromFileURLs={true}
+          allowUniversalAccessFromFileURLs={true}
+          mixedContentMode={"always"}
           // 웹뷰 뒤로가기 관련
           injectedJavaScript={`
 
@@ -144,9 +187,14 @@ export default function App() {
 
       `}
           onMessage={({ nativeEvent: state }) => {
+            console.log(state);
             if (state.data === "navigationStateChange") {
               // Navigation state updated, can check state.canGoBack, etc.
               setIsCanGoBack(state.canGoBack);
+            }
+
+            if (state.data.type === "download") {
+              SaveToPhone(data.url);
             }
           }}
         />
