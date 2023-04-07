900ae463ae9effaf20b8048ab3d0f2af3df725d4
Dakyung Seo <seoda0000@gmail.com>
2023-04-07 12:59:27 +0900

    Feat: 캡쳐 기능 구현

--- front-end/flory-app/App.js
+++ front-end/flory-app/App.js
@@ -9,7 +9,7 @@ import {
   View,
 } from "react-native";
 import { WebView } from "react-native-webview";
-import { BackHandler } from "react-native";
+import { BackHandler, Alert } from "react-native";
 import * as Location from "expo-location";
 import * as MediaLibrary from "expo-media-library";
 import * as FileSystem from "expo-file-system";
@@ -86,10 +86,11 @@ export default function App() {
 
   // save image
   const SaveToPhone = async (url) => {
-    console.log(url);
-    Toast.show("image download", {
-      duration: 1000,
-    });
+    // console.log("zzzzzzzzzzzzzzzzzzz", url);
+    // Toast.show("image download", {
+    //   duration: 1000,
+    // });
+    console.log("image download");
     const { uri } = await FileSystem.downloadAsync(
       url,
       `${FileSystem.documentDirectory}meme.jpg`
@@ -104,9 +105,9 @@ export default function App() {
         MediaLibrary.createAlbumAsync("Images", asset, false)
           .then(() => {
             console.log("File Saved Successfully!");
-            Toast.show("image save success!!", {
-              duration: 1000,
-            });
+            // Toast.show("image save success!!", {
+            //   duration: 1000,
+            // });
           })
           .catch(() => {
             console.log("Error In Saving File!");
@@ -119,18 +120,34 @@ export default function App() {
     }
   };
 
+  const SaveImage = async (base64Code) => {
+    // console.log(typeof base64Code);
+    const permission = await MediaLibrary.requestPermissionsAsync();
+    if (permission.granted) {
+      const filename = FileSystem.documentDirectory + "bloomer.png";
+
+      await FileSystem.writeAsStringAsync(filename, base64Code, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      const mediaResult = await MediaLibrary.saveToLibraryAsync(filename);
+      Alert.alert("", "이미지가 갤러리에 저장되었습니다");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={{ width: "100%", height: "100%" }}>
         <WebView
           ref={webview}
-          source={{ uri: "http://j8a205.p.ssafy.io/" }}
+          source={{ uri: "https://j8a205.p.ssafy.io/" }}
           javaScriptEnabled={true}
           onLoad={console.log("Loaded!")}
           domStorageEnabled={true}
           geolocationEnabled={true}
           style={{ zIndex: 1 }}
           useWebkit
+          // 구글 로그인 관련
+          userAgent="customUserAgent"
           // 캡쳐 저장 관련
           allowFileAccess={true}
           allowFileAccessFromFileURLs={true}
@@ -187,14 +204,26 @@ export default function App() {
 
       `}
           onMessage={({ nativeEvent: state }) => {
-            console.log(state);
+            // console.log("데이터", typeof state.data);
             if (state.data === "navigationStateChange") {
               // Navigation state updated, can check state.canGoBack, etc.
               setIsCanGoBack(state.canGoBack);
-            }
+            } else {
+              // console.log("데이터를 파싱해요", JSON.parse(state.data));
+              // console.log("데이터를 파싱 타입", typeof JSON.parse(state.data));
+              // console.log("데이터를 파싱의 type", JSON.parse(state.data).type);
+              // console.log("데이터를 파싱의 data", JSON.parse(state.data).data);
+              const base64Image = JSON.parse(state.data).data;
+              // console.log("there", base64Image);
 
+              var base64Code = base64Image.split("data:image/png;base64,");
+              // console.log("here", base64Code);
+              // console.log("here", typeof base64Code);
+              SaveImage(base64Code[1]);
+              // SaveToPhone(JSON.parse(state.data).data);
+            }
             if (state.data.type === "download") {
-              SaveToPhone(data.url);
+              // SaveToPhone(state.data);
             }
           }}
         />
